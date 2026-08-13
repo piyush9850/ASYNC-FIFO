a61f465026fa6959e07ce56b2c79038372f35f07
piyush9850 <piyushchoudhary0905@gmail.com>
2026-08-13 17:51:18 +0530

    write handler pointer

--- wptr_handler.v
+++ wptr_handler.v
@@ -6,11 +6,13 @@ module wptr_handler #(
     input w_rst,
     input [$clog2(DEPTH):0] g_rptr_sync, // depth bit + 1 msb bit 
 
-    output [$clog2(DEPTH):0] g_wptr,
-    output full,
+    output reg [$clog2(DEPTH):0] g_wptr,
+    output reg full,
     output [$clog2(DEPTH)-1:0] wr_addr
 
 );
+
+wire wfull;
 
 reg [$clog2(DEPTH):0] b_wptr; 
 
@@ -20,11 +22,39 @@ reg [$clog2(DEPTH):0] g_wptr_next; // gray pointer next
 
 
 
- assign b_wptr_next = ( b_wptr + ( wclk && ~full) ); // increment of binary pointer
+ assign b_wptr_next = ( b_wptr + ( write_en && ~full) ); // increment of binary pointer
 
 assign g_wptr_next = (b_wptr_next >> 1) ^ b_wptr_next; // conversion of binary to gray 
 
+assign wr_addr = b_wptr[$clog2(DEPTH)-1:0];
 
+  assign wfull =
+        (g_wptr_next ==
+        {
+            ~g_rptr_sync[$clog2(DEPTH):$clog2(DEPTH)-1],
+             g_rptr_sync[$clog2(DEPTH)-2:0]
+        });
+
+
+
+always @(posedge wclk or negedge w_rst) begin
+    
+    if(!w_rst)
+    begin
+        b_wptr <= 0;
+        g_wptr <= 0;
+        full <= 0;
+
+    end
+    else 
+    begin
+        b_wptr <= b_wptr_next;
+        g_wptr <= g_wptr_next;
+        full <= wfull;
+    end
+
+
+end
 
     
 endmodule

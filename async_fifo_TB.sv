15cd1af12a693bc18dedc106ef10f20cdac05632
piyush9850 <piyushchoudhary0905@gmail.com>
2026-08-26 10:40:55 +0530

    write task done

--- async_fifo_TB.sv
+++ async_fifo_TB.sv
@@ -21,7 +21,78 @@ logic empty;
 
 logic [WIDTH-1:0] expected_q[$];
 
-top_module dut (.wclk(wclk), .w_rst(w_rst), .write_en(write_en), .data_in(data_in), .rclk(rclk), .r_rst(r_rst), .read_en(read_en), .full(full), .empty(empty), .data_out(data_out) );
+top_module #(
+    .WIDTH(WIDTH),
+    .DEPTH(DEPTH)
+) dut (
+    .wclk(wclk),
+ .w_rst(w_rst),
+  .write_en(write_en),
+   .data_in(data_in),
+    .rclk(rclk),
+     .r_rst(r_rst),
+      .read_en(read_en),
+       .full(full),
+        .empty(empty),
+         .data_out(data_out) 
+         );
+
+
+initial begin  // initialion of wclk with period 10
+wclk = 0;
+    forever begin
+        #5;
+wclk = ~wclk;
+   end
+
+end
+
+initial begin // initialion of rclk with period 14
+    rclk = 0;
+forever begin
+#7;
+rclk = ~rclk;
+end
+end
+
+initial begin  // reset condition
+    
+    w_rst = 0;
+    r_rst = 0;
+
+    write_en = 0;
+    read_en = 0;
+
+    data_in = 0;
+
+    #50;
+
+    w_rst = 1;
+    r_rst = 1;
+
+
+end
+
+task automatic write_byte(input logic [WIDTH-1:0] val);  // write task
+
+@(posedge wclk);
+begin
+    if(!full)
+    begin
+        write_en = 1;
+        data_in = val;
+        expected_q.push_back(data_in);
+    end
+end
+
+@(posedge  wclk);
+begin
+write_en = 0;
+end
+
+
+endtask
+
 
 
 
